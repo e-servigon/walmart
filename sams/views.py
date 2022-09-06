6acb484d6e26f3f9fee67a55864fb4b17f054911
Anthony <anthonygovea21@gmail.com>
2022-09-05 21:09:27 -0500

    message

--- sams/views.py
+++ sams/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from urllib.request import Request
+from django.shortcuts import render, redirect
 from .forms import * 
 from .models import *
 from django.contrib.auth import authenticate, login 
@@ -7,11 +8,25 @@ from django.contrib.auth import authenticate, login
 def loginuser(request):
     login_form = AutheticationUserForm()
     contexto = {'login_form': login_form}
+    if request.method == 'POST':
+        login_form = AutheticationUserForm(request.POST)
+        user = request.POST.get('username')
+        password2 = request.POST.get('password')
+        user_authenticate = authenticate(request, username = user, password = password2)
+        if user_authenticate is not None:
+            login(request, user_authenticate)
+            return redirect('home')
     return render(request,'login.html',contexto)
 
 def createuser (request):
     user_form = CreateUserForm()
     contexto = {'user_form': user_form}
+    if request.method== 'POST':
+        print(request.POST)
+        user_form = CreateUserForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')
     return render(request,'createuser.html',contexto)
 
 def home (request):
--- walmart/urls.py
+++ walmart/urls.py
@@ -19,7 +19,7 @@ from sams import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.loginuser),
+    path('login/', views.loginuser, name = 'login'),
     path('newuser/',views.createuser, name= 'createuser'),
     path('logout/', views.logoutuser),
     path('sams/',include('sams.urls'))
