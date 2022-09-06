a9c38f21cd15ad1fe352dc8d12bf172f9a9e2bc2
Anthony <anthonygovea21@gmail.com>
2022-09-05 21:20:14 -0500

    cambio

--- sams/views.py
+++ sams/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from .forms import * 
 from .models import *
 from django.contrib.auth import authenticate, login 
-# Create your views here.
+
 
 def loginuser(request):
     login_form = AutheticationUserForm()
