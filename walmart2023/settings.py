2608e681e5fa2760d71c8465254c8356cd41db7d
Ernesto Servigon <ernestoservigon@lasallistas.org.mx>
2024-11-11 16:37:17 -0600

    apuntes de estaticos y cloud database

--- walmart2023/settings.py
+++ walmart2023/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'walmart2023.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': '',  
+        'USER': '',  
+        'PASSWORD': '',  
+        'HOST': 'testdb-e-servigon.h.aivencloud.com',  
+        'PORT': '17459',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" 
+        }
     }
 }
 
